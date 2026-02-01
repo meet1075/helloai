7f14c2a8323add56542240f6ac41a518ca222bce
meet1075 <meetsoni1075@gmail.com>
2026-02-01 14:32:09 +0530

    dashboard sidebar

---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,118 @@
+"use client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import { VideoIcon, BotIcon, icons, StarIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import DashboardUserButton from "./dashboard-user-button";
+
+const firstSection = [
+  {
+    icon: VideoIcon,
+    label: "Meetings",
+    href: "/meetings",
+  },
+  {
+    icon: BotIcon,
+    label: "Agents",
+    href: "/agents",
+  },
+];
+const SecondSection = [
+  {
+    icon: StarIcon,
+    label: "Upgrade",
+    href: "/upgrade",
+  },
+];
+
+const DashboardSidebar = () => {
+  const pathname = usePathname();
+  return (
+    <Sidebar>
+      <SidebarHeader className="text-sidebar-accent-foreground">
+        <Link href="/" className="flex items-center gap-2 px-2 pt-2">
+          <Image src="./logoo.svg" alt="Hello.AI" height={36} width={36} />
+          <p className="text-2xl font-semibold">Hello.AI</p>
+        </Link>
+      </SidebarHeader>
+      <div>
+        <Separator className="opacity-10 text-[#5D6B68]" />
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B68]/10",
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <div>
+          <Separator className="opacity-10 text-[#5D6B68]" />
+        </div>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {SecondSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B68]/10",
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+			<SidebarFooter>
+				<DashboardUserButton />
+			</SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default DashboardSidebar;
