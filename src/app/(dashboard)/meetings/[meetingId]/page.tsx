48cdbcfbfe521f1f1b59462f32a0997defb879be
meet1075 <meetsoni1075@gmail.com>
2026-02-06 00:59:43 +0530

    meetings setup

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      meetings id page 
+    </div>
+  )
+}
+
+export default page
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,8 +2,10 @@ import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
 import { agentsRouter } from '@/modules/agents/server/procedure';
 import { agents } from '@/db/schema';
+import { meetingRouter } from '@/modules/meetings/server/procedure';
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings:meetingRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
